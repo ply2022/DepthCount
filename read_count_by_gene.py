5a37c4ec32fddc893484398134731c1c8212d5f5
NanoSeqWrokshop <98194829+ply2022@users.noreply.github.com>
2023-05-02 18:04:52 -0400

    Update read_count_by_gene.py
    Add calculation of enrichment

--- read_count_by_gene.py
+++ read_count_by_gene.py
@@ -1,10 +1,11 @@
 ###########################################
 # Author: Jose Huguet-Tapia               #
 # Email: jhuguet@ufl.edu                  #
-# Last modified: 2023/04/27; Pei-Ling Yu  #
+# Last modified: 2023/05/02; Pei-Ling Yu  #
 ###########################################
-
 import subprocess
+import pandas as pd
+
 # Define input and ouput files
 input_file = "overlapped_gene" 
 input_bam = "BAM_file"
@@ -21,3 +22,14 @@ with open(input_file, 'r') as f_in, open(output_bed, 'w') as f_out:
 
 # Calculate coverage of each region using samtools bedcov
 subprocess.run(["samtools", "-c","bedcov", sorted_bed, input_bam], stdout=open(cov_output, 'w'), check=True)
+
+# Read the coverage.txt file into a pandas dataframe
+coverage = pd.read_csv('coverage.txt', sep='\t', header=None)
+
+# Calculate the enrichment factor
+genome_size = <Define genome size>
+coverage['Enrichment'] = (coverage[4] / coverage[5]) / ((coverage[2] - coverage[1]) / genome_size)
+
+# Write the updated dataframe to a new coverage file with the enrichment factor
+coverage.to_csv('coverage_enrichment.txt', sep='\t', index=False, header=False)
+
